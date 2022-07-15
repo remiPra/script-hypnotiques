9f54f87d171257910b06ebb567d0c1882c795701
remiPra <remipradere@gmail.com>
2022-07-16 00:00:06 +0200

    ZEZE

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
 
 module.exports = nextConfig
